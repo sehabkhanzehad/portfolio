0fa5116e1be529272b87000732f0078c741db493
Sk Zehad <sehabkhanzehad@yahoo.com>
2026-01-13 01:37:40 +0600

    fix: update gradient class to use linear gradient for footer

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
         <Footer />
       </main>
 
-      <div className="fixed bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-background via-background/80 to-transparent pointer-events-none"></div>
+      <div className="fixed bottom-0 left-0 right-0 h-24 bg-linear-to-t from-background via-background/80 to-transparent pointer-events-none"></div>
     </div>
   )
 }
